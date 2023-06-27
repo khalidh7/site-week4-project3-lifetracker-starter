79769188410c73c9986d3102322991ecdde9c087
khalidh7 <103854400+khalidh7@users.noreply.github.com>
2023-06-27 11:12:32 -0700

    basic routing

---
+++ lifetracker-ui/src/components/ActivityPage/ActivityPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ActivityPage.css";
+
+export default function ActivityPage() {
+    return (
+        <div>
+        <h1>Activity Page</h1>
+        </div>
+    );
+    }
--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -4,14 +4,24 @@ import Navbar from '../Navbar/Navbar'
 import Home from '../Home/Home'
 import Login from '../Login/Login'
 import Signup from '../Signup/Signup'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import ActivityPage from '../ActivityPage/ActivityPage'
 
 function App() {
 
   return (
-    <div>
-      <Navbar />
-      <Signup />
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/activity" element={<ActivityPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/register' element={<Signup />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
