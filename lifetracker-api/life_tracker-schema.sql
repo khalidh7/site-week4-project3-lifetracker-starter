d7b7e1ec0a991bc5898048c7c87744c6fcb77826
khalidh7 <103854400+khalidh7@users.noreply.github.com>
2023-06-28 13:11:46 -0700

    creating new table

--- lifetracker-api/life_tracker-schema.sql
+++ lifetracker-api/life_tracker-schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
   password   TEXT NOT NULL,
   fullname  TEXT NOT NULL,
   email      TEXT NOT NULL UNIQUE CHECK (position('@' IN email) > 1));
+
+CREATE TABLE activity (
+  id       SERIAL PRIMARY KEY,
+  userid    int REFERENCES users(id),
+  date      TEXT NOT NULL,
+  starttime TEXT NOT NULL,
+  endtime   TEXT NOT NULL,
+  rating    TEXT NOT NULL);
--- lifetracker-ui/src/components/Login/Login.jsx
+++ lifetracker-ui/src/components/Login/Login.jsx
@@ -2,25 +2,32 @@ import React from "react";
 import "./Login.css";
 
 export default function Login() {
+
+  function handleOnSubmit(){
+    // let email = document.getElementById("email").value;
+    // let password = document.getElementById("password").value;
+    // console.log(email);
+  }
+
     return (
     <div className="login-container">
       <h1>Login</h1>
       <form className="form">
         <div className="form-group">
-          <label htmlFor="email">Email:</label>
+          <label className="email-login" htmlFor="email">Email:</label>
           <input
             type="email"
             id="email"
           />
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password:</label>
+          <label className="password-login" htmlFor="password">Password:</label>
           <input
             type="password"
             id="password"
           />
         </div>
-        <button type="submit" className="submit-button">Login</button>
+        <button type="submit" className="submit-button" onClick={handleOnSubmit()}>Login</button>
       </form>
     </div>
     );
